897de844f76745951616943bcde9dd8c188565a6
Mert <mertuygur02@gmail.com>
2021-07-10 10:08:36 +0300

    login

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,8 +1,11 @@
 const pool = require('../db');
 const bcrypt = require('bcrypt');
 const router = require('express').Router();
+const jwtGenerator = require('../utils/jwtGenerator');
+const validInfo = require('../middleware/validInfo');
+const authorization = require('../middleware/authorization');
 
-router.post('/', async(req, res) => {
+router.post('/register', validInfo, async(req, res) => {
   try {
     const {name, email, password} = req.body;
 
@@ -16,11 +19,44 @@ router.post('/', async(req, res) => {
     const bcryptPassword = await bcrypt.hash(password, salt);
 
     const newUser = await pool.query('INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *;', [name, email, bcryptPassword]);
-    res.json(newUser);
+
+    const token = jwtGenerator(newUser.rows[0].user_id);
+
+    res.json({token});
 
   } catch (error) {
     console.error(error.message);
     res.status(500).send('JWT Auth error.');
+  }
+})
+
+//login
+router.post('/login', validInfo, async(req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [email]);
+    if(user.rows.length === 0){
+      return res.status(401).send('Password or Email is incorrect.')
+    }
+    const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+    if(!validPassword){
+      return res.status(401).json('Password or Email is incorrect.');
+    }
+
+    const token = jwtGenerator(user.rows[0].user_id);
+    res.json({token});
+  } catch (error) {
+    console.error(error.message, 'Login Error');
+  }
+});
+
+router.get('/is-verify', authorization, async(req, res) => {
+  try {
+    res.json(true);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error.');
   }
 })
 
