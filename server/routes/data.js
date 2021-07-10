767eab0d3481abd8d9ccfa5eb9f133eb513deb8f
Mert <mertuygur02@gmail.com>
2021-07-10 17:10:11 +0300

    EVERYTHING WORKS LIKE A DREAM

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,9 +1,16 @@
 const pool = require('../db');
 const router = require('express').Router();
+const authorization = require('../middleware/authorization');
 
-router.post('/', async(req, res) => {
-  const {work, leisure, game, happiness} = req.body;
-  
-})
+router.post('/', authorization, async(req, res) => {
+  try {
+    const {work, leisure, game, happiness} = req.body;
+    const newData = await pool.query('INSERT INTO parameters (owner_id, work, leisure, game, happiness) VALUES ($1, $2, $3, $4, $5);', [req.user, work, leisure, game, happiness]);
+    res.json(newData);
+
+  } catch (error) {
+    console.error(error.message, "Data posting route.");
+  }
+});
 
 module.exports = router;
