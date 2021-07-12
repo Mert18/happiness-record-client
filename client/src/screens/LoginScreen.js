6e20aba28c3b5d2520c7d50ca30e442a9912a0b2
Mert <mertuygur02@gmail.com>
2021-07-13 00:15:32 +0300

    register and login forms styled

--- client/src/screens/LoginScreen.js
+++ client/src/screens/LoginScreen.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import Layout from '../core/Layout'
-
+import '../styles/forms.css'
 const LoginScreen = () => {
   return (
     <Layout>
-      <div className="form-container">
-        <form className="form">
-          <div className="formgroup">
-            <label htmlFor='email'>E-mail</label>
-            <input type='text' id='email' />
-          </div>
-
-          <div className="formgroup">
-            <label htmlFor='password'>Password</label>
-            <input type='text' id='password' />
-          </div>
-
-          <button>Submit</button>
+      <div className="form-container" id="logcon">
+        <form className="form" id="logform">
+            <h2>Login</h2>
+            <input type='text' id='email' placeholder="email" />
+            <input type='text' id='password' placeholder="password" />
+            <button id="logbut"></button>
         </form>
       </div>
     </Layout>
--- client/src/screens/RegisterScreen.js
+++ client/src/screens/RegisterScreen.js
@@ -5,29 +5,14 @@ import '../styles/forms.css';
 const RegisterScreen = () => {
   return (
     <Layout>
-      <div className="form-container">
-        <form className="form">
-          <div className="formgroup">
-            <label htmlFor='username'>Username</label>
-            <input type='text' id='username' />
-          </div>
-
-          <div className="formgroup">
-            <label htmlFor='email'>E-mail</label>
-            <input type='text' id='email' />
-          </div>
-
-          <div className="formgroup">
-            <label htmlFor='password'>Password</label>
-            <input type='text' id='password' />
-          </div>
-
-          <div className="formgroup">
-            <label htmlFor='password2'>Re-Password</label>
-            <input type='text' id='password2' />
-          </div>
-
-          <button>Submit</button>
+      <div className="form-container" id="regcon">
+        <form className="form" id="regform">
+            <h2>Register</h2>
+            <input type='text' id='username' placeholder="username" />
+            <input type='text' placeholder="email" id='email' />
+            <input type='text' placeholder="password" id='password' />
+            <input type='text' placeholder="password" id='password2' />
+          <button id="regbut"></button>
         </form>
       </div>
     </Layout>
