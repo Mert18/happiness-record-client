140df82872366dee906825ec1b471b0acaf8ac0f
Mert <mertuygur02@gmail.com>
2021-07-14 13:52:49 +0300

    lalallalalalalalalla

--- client/src/screens/Dashboard.js
+++ client/src/screens/Dashboard.js
@@ -10,6 +10,32 @@ const Dashboard = ({ setAuth }) => {
   const [game, setGame] = useState(0);
   const [leisure, setLeisure] = useState(0);
   const [happiness, setHappiness] = useState(0);
+  const [message, setMessage] = useState("")
+  const [day, setDay] = useState('');
+
+  const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = today.getFullYear();
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { work, game, leisure, happiness };
+      const response = await fetch(
+        "http://localhost:5000/data/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            jwt_token: localStorage.token},
+          body: JSON.stringify(body)
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
   const getProfile = async () => {
     try {
@@ -37,7 +63,9 @@ const Dashboard = ({ setAuth }) => {
 
   useEffect(() => {
     getProfile();
+    
   }, []);
+  
   return (
     <Layout>
       <div className="dashboard">
@@ -45,32 +73,33 @@ const Dashboard = ({ setAuth }) => {
           <h1>{name}</h1>
           <button onClick={e => logout(e)}>logout</button>
         </div>
-
+        <div>
+          {message}
+        </div>
         <div className="dashboard-data">
           <div className="dashboard-data-today">
-            <h2>July 13, 2021</h2>
+            <h2>{mm}/{dd}/{yyyy}</h2>
           </div>
-          <form className="dashboard-data-inp">
+          <form className="dashboard-data-inp" onSubmit={onSubmitForm}>
             <div className="inp-group">
               <label>Work</label>
-              <input id="work" type="range" min="0" max="100" value={work} onChange={(e) => setWork(e.target.value)} />
+              <input id="work" type="number" step="5" min="0" max="100" value={work} onChange={(e) => {setWork(e.target.value)}} />
             </div>
             <div className="inp-group">
               <label>Game</label>
-              <input id="game" type="range" min="0" max="100" value={game} onChange={(e) => setGame(e.target.value)} />
+              <input id="game" type="number" min="0" step="5" max="100" value={game} onChange={(e) => {setGame(e.target.value)}} />
             </div>
             <div className="inp-group">
               <label>Leisure</label>
-              <input id="leisure" type="range" min="0" max="100" value={leisure} onChange={(e) => setLeisure(e.target.value)} />
+              <input id="leisure" type="number" min="0" step="5" max="100" value={leisure} onChange={(e) => {setLeisure(e.target.value)}} />
             </div>
             <div className="inp-group">
               <label>Happiness</label>
-              <input id="happiness" type="range" min="0" max="100" value={happiness} onChange={(e) => setHappiness(e.target.value)} />
+              <input id="happiness" type="number" min="0" step="5" max="100" value={happiness} onChange={(e) => setHappiness(e.target.value)} />
             </div>
             <button>Send</button>
           </form>
         </div>
-        
         <div className="dashboard-graph">
           GRAPH HERE
         </div>
--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use("/authentication", require("./routes/jwtAuth"));
 
 app.use("/dashboard", require("./routes/dashboard"));
 
+app.use("/data", require("./routes/data"));
+
 app.listen(5000, () => {
   console.log(`Server is starting on port 5000`);
 });
