24677a3375c5ee45d06dd0675f5999a4cef07fea
Mert18 <mertuygur02@gmail.com>
2022-01-31 13:22:17 +0300

    register components created

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dda68faa50212b8d"; }
+/******/ 		__webpack_require__.h = function() { return "3640d88e3477426f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,7 +1,55 @@
 import classes from './Register.module.css'
+import { useState } from 'react'
 
 const Register = () => {
-  return <div className={classes.register}></div>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordAgain, setPasswordAgain] = useState('')
+  return (
+    <div className={classes.register}>
+      <div className={classes.register__form}>
+        <form className={classes.form}>
+          <div className={classes.inputs}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+
+          <div className={classes.inputs}>
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className={classes.inputs}>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className={classes.inputs}>
+            <label htmlFor="passwordagain">Password Again</label>
+            <input
+              type="password"
+              id="passwordagain"
+              onChange={(e) => setPasswordAgain(e.target.value)}
+            />
+          </div>
+        </form>
+      </div>
+      <div className={classes.register__ills}></div>
+    </div>
+  )
 }
 
 export default Register
